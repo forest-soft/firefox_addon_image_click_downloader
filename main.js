33b759c4c173699253632765c9532d3e97e0b1a0
森林ソフト <kimoto@forest-soft.net>
2021-11-25 01:32:02 +0900

    動的要素対応 + クリックイベントを上書きできるように対応中

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 画像保存処理
 function save_image(event) {
 	function handleResponse(message) {
-		console.log(`Message from the background script:  ${message.response}`);
+		// console.log(`Message from the background script:  ${message.response}`);
 	}
 	
 	function handleError(error) {
@@ -14,11 +14,170 @@ function save_image(event) {
 	sending.then(handleResponse, handleError);
 }
 
+/*
+// 初期の処理
 var image_list = document.getElementsByTagName("img");
 for (var i = 0; i < image_list.length; i++) {
 	var image_element = image_list[i];
 	image_element.addEventListener("dblclick", save_image);
 }
+*/
+
+
+/*
+// 動的に追加した要素に対応するバージョン
+window.addEventListener("dblclick", function(event) {
+	if (event.srcElement.nodeName == "IMG") {
+		save_image(event);
+	}
+});
+*/
+
+
+// 設定値の取得処理
+function onError(e) {
+	console.error(e);
+}
+
+var setting = {
+	save_dir: "image",
+	is_show_popup_by_start: "yes",
+	is_show_thumbnail_by_start: "no",
+	is_show_popup_by_complete: "no",
+	is_show_thumbnail_by_complete: "no",
+	is_overwrite_click_event: "no",
+	overwrite_click_double_click_speed: "300",
+	overwrite_click_event_site_domain: ""
+};
+
+function onGot(storage_data) {
+	for (key in storage_data) {
+		setting[key] = storage_data[key];
+	}
+	
+	setting.overwrite_click_double_click_speed = parseInt(setting.overwrite_click_double_click_speed, 10);
+	setting.overwrite_click_event_site_domain = setting.overwrite_click_event_site_domain.toLowerCase().split("\n");
+	setting.overwrite_click_event_site_domain = setting.overwrite_click_event_site_domain.filter(Boolean);
+	
+	// 現在見ているサイトが「クリックイベントを上書きするサイトのドメイン名」に含まれていなければクリックイベントを上書きしない。
+	if (setting.overwrite_click_event_site_domain.length != 0 && !setting.overwrite_click_event_site_domain.includes(document.domain.toLowerCase())) {
+		setting.is_overwrite_click_event = "no";
+	}
+	
+	let is_browser_image_view = false;
+	if (document && document.body && document.body.childNodes.length == 1 && document.body.childNodes[0].tagName == "IMG") {
+		is_browser_image_view = true;
+	}
+	
+	if (is_browser_image_view || setting.is_overwrite_click_event == "no") {
+		window.addEventListener("dblclick", function(event) {
+			if (event.srcElement.nodeName == "IMG") {
+				save_image(event);
+			}
+		});
+	} else {
+		console.log("クリックイベント上書き！");
+		
+		const overwrite_click_event = function(event) {
+			console.log("クリックした要素");
+			console.log(event.target);
+			
+			if (event.target.nodeName != "IMG") {
+				return true;
+			}
+			console.log("乗っ取りX");
+
+			console.log(event.target.dataset.xxx_click);
+			/*
+			event.preventDefault();
+			event.stopPropagation();
+			event.stopImmediatePropagation();
+			*/
+			if ((typeof event.target.dataset.xxx_click === "undefined") || (event.target.dataset.xxx_click == "end")) {
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+				event.target.dataset.xxx_click = "start";
+				console.log("single start");
+				
+				setTimeout(function () {
+					if (event.target.dataset.xxx_click == "start") {
+						console.log("single click!");
+						event.target.dataset.xxx_click = "single";
+						event.target.click();
+					}
+				}, setting.overwrite_click_double_click_speed);
+				
+				return false;
+			} else if (event.target.dataset.xxx_click == "single") {
+				// シングルクリック
+				console.log("single通常");
+				event.target.dataset.xxx_click = "end";
+				
+				return true;
+			} else {
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+				event.target.dataset.xxx_click = "end";
+				console.log("ダブルクリック");
+				save_image(event);
+				
+				return false;
+			}
+			
+			return false;
+		}
+		
+		// キャプチャーフェーズでクリックを補足してクリックイベントを調整する。
+		window.addEventListener("click", overwrite_click_event, true);
+/*
+		// 元々ある要素にクリックイベント割り当て
+		const image_list = document.getElementsByTagName("img");
+		for (let i = 0; i < image_list.length; i++) {
+			const image_element = image_list[i];
+			image_element.addEventListener("click", overwrite_click_event);
+		}
+		
+		// 追加された要素へのクリックイベントの割り当て
+		const attributes_area_observer = new MutationObserver((mutations) => {
+			console.log("追加された要素");
+			mutations.forEach((mutation) => {
+				
+				for (let i = 0; i < mutation.addedNodes.length; i++) {
+					if (typeof mutation.addedNodes[i].getElementsByTagName === "undefined") {
+						continue;
+					}
+					
+					console.log(mutation.addedNodes[i]);
+					
+					if (mutation.addedNodes[i].nodeName == "IMG") {
+						mutation.addedNodes[i].removeEventListener("click", overwrite_click_event);
+						mutation.addedNodes[i].addEventListener("click", overwrite_click_event);
+					} else {
+						const image_list = mutation.addedNodes[i].getElementsByTagName("img");
+						for (let i = 0; i < image_list.length; i++) {
+							const image_element = image_list[i];
+							image_element.removeEventListener("click", overwrite_click_event);
+							image_element.addEventListener("click", overwrite_click_event);
+						}
+					}
+				}
+			});
+		});
+
+		var attributes_area_observer_config = {
+			childList: true,
+			subtree: true,
+		};
+
+		attributes_area_observer.observe(document.getElementsByTagName("body")[0], attributes_area_observer_config);
+*/
+	}
+}
+
+var getting = browser.storage.sync.get();
+getting.then(onGot, onError);
 
 
 /*
--- options_ui/options.js
+++ options_ui/options.js
@@ -4,19 +4,38 @@ for (var i = 0; i < label_list.length; i++) {
 	while (label_list[i].firstChild) {
 		label_list[i].removeChild(label_list[i].firstChild);
 	}
-	label_list[i].appendChild(document.createTextNode(browser.i18n.getMessage(label_list[i].className)));
+	const class_name = label_list[i].className.split(" ")[0];
+	label_list[i].appendChild(document.createTextNode(browser.i18n.getMessage(class_name)));
 }
 
 function saveOptions(e) {
 	e.preventDefault();
+	
+	// 特殊機能設定(ダブルクリック速度)を整数値以外のものが入力されていればデフォルト値に戻す。
+	let overwrite_click_double_click_speed = document.querySelector("#overwrite_click_double_click_speed").value;
+	overwrite_click_double_click_speed = parseInt(overwrite_click_double_click_speed, 10);
+	if (!Number.isInteger(overwrite_click_double_click_speed) || overwrite_click_double_click_speed == 0) {
+		overwrite_click_double_click_speed = 300;
+	}
+	overwrite_click_double_click_speed = String(overwrite_click_double_click_speed);
+	
+	let overwrite_click_event_site_domain = document.querySelector("textarea[name=overwrite_click_event_site_domain]").value.split("\n");
+	for (let i = 0; i < overwrite_click_event_site_domain.length; i++) {
+		overwrite_click_event_site_domain[i] = overwrite_click_event_site_domain[i].trim();
+	}
+	overwrite_click_event_site_domain = overwrite_click_event_site_domain.join("\n");
 	
 	browser.storage.sync.set({
 		save_dir: document.querySelector("#save_dir").value,
 		is_show_popup_by_start: document.querySelector("input[name=is_show_popup_by_start]:checked").value,
 		is_show_thumbnail_by_start: document.querySelector("input[name=is_show_thumbnail_by_start]:checked").value,
 		is_show_popup_by_complete: document.querySelector("input[name=is_show_popup_by_complete]:checked").value,
-		is_show_thumbnail_by_complete: document.querySelector("input[name=is_show_thumbnail_by_complete]:checked").value
+		is_show_thumbnail_by_complete: document.querySelector("input[name=is_show_thumbnail_by_complete]:checked").value,
+		is_overwrite_click_event: document.querySelector("input[name=is_overwrite_click_event]:checked").value,
+		overwrite_click_double_click_speed: overwrite_click_double_click_speed,
+		overwrite_click_event_site_domain: overwrite_click_event_site_domain
 	}).then(function(){
+		restoreOptions();
 		alert(browser.i18n.getMessage("option_label_save_complete"));
 	}, function(error){
 		alert(browser.i18n.getMessage("option_label_save_error") + "\n" + error);
@@ -26,20 +45,35 @@ function saveOptions(e) {
 function restoreOptions() {
 
 	function setCurrentChoice(result) {
-		
+		// 保存ディレクトリ名
 		document.querySelector("#save_dir").value = "save_dir" in result ? result.save_dir : "image";
 		
+		// ダウンロード開始ポップアップ設定(ポップアップ表示)
 		result.is_show_popup_by_start = result.is_show_popup_by_start || "yes";
 		document.querySelector("input[name=is_show_popup_by_start][value=" + result.is_show_popup_by_start + "]").checked = true;
 		
+		// ダウンロード開始ポップアップ設定(保存画像のサムネイル表示)
 		result.is_show_thumbnail_by_start = result.is_show_thumbnail_by_start || "no";
 		document.querySelector("input[name=is_show_thumbnail_by_start][value=" + result.is_show_thumbnail_by_start + "]").checked = true;
 		
+		// ダウンロード完了ポップアップ設定(ポップアップ表示)
 		result.is_show_popup_by_complete = result.is_show_popup_by_complete || "no";
 		document.querySelector("input[name=is_show_popup_by_complete][value=" + result.is_show_popup_by_complete + "]").checked = true;
 		
+		// ダウンロード完了ポップアップ設定(保存画像のサムネイル表示)
 		result.is_show_thumbnail_by_complete = result.is_show_thumbnail_by_complete || "no";
 		document.querySelector("input[name=is_show_thumbnail_by_complete][value=" + result.is_show_thumbnail_by_complete + "]").checked = true;
+		
+		// 特殊機能設定(クリックイベントの上書き)
+		result.is_overwrite_click_event = result.is_overwrite_click_event || "no";
+		document.querySelector("input[name=is_overwrite_click_event][value=" + result.is_overwrite_click_event + "]").checked = true;
+		
+		// 特殊機能設定(ダブルクリック速度)
+		document.querySelector("#overwrite_click_double_click_speed").value = "overwrite_click_double_click_speed" in result ? result.overwrite_click_double_click_speed : "300";
+		
+		// 特殊機能設定(クリックイベントを上書きするサイトのドメイン名)
+		result.overwrite_click_event_site_domain = result.overwrite_click_event_site_domain || "";
+		document.querySelector("textarea[name=overwrite_click_event_site_domain]").value = result.overwrite_click_event_site_domain;
 	}
 
 	function onError(error) {
